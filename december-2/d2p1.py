b7a86ebb96127a58fab15bb57ead46fa7cd6a77e
= <hnytun@gmail.com>
2018-12-04 04:21:42 +0100

    finished day 2, part 1 and 2

--- december-2/d2p1.py
+++ december-2/d2p1.py
@@ -2,7 +2,8 @@
 """
 Gets checksum of a list of IDs
 """
-def getChecksum(inputList):
+def getChecksum(inputFile):
+    inputList = makeListFromInput(inputFile)
     twice = 0
     thrice = 0
     for ID in inputList:
@@ -27,4 +28,44 @@ def makeListFromInput(inputFile):
         inList.append(line.rstrip())
     return inList
 
-print(getChecksum(makeListFromInput("input.txt")))
+
+
+"""
+PART 2
+"""
+
+"""
+Find indices of different letters on two strings, assuming they are the same size
+- for this specific task our desired length of the indices list should be 1
+"""
+
+def findNumDiff(s1,s2):
+    letters1 = list(s1)
+    letters2 = list(s2)
+    indices = list()
+    count=0
+    diff = 0
+    while(count<len(letters1)):
+        if letters1[count] != letters2[count]:
+            indices.append(count)
+        count+=1
+    return indices
+
+
+"""
+Function to find one of the correct boxes by finding other boxes with an ID that differs in 1 character.
+To find the common letters we then remove the differenting character
+"""
+
+def findCommonLetters(inputFile):
+    inputList = makeListFromInput(inputFile)
+    for ID in inputList:
+        for otherID in inputList:
+            if otherID != ID and len(findNumDiff(ID,otherID)) == 1:
+                index = findNumDiff(ID,otherID)[0]
+                return ID[:index] + ID[index+1:]
+
+print("checksum: " + str(getChecksum("input.txt")))
+
+
+print("common ID of correct boxes: " + str(findCommonLetters("input.txt")))
